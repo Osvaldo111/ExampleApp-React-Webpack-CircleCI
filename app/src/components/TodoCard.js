16df4a80e1182a5387f45e07c8f47d8237b835f6
Osvaldo <osvaldofabricio11@gmail.com>
2020-04-25 17:00:27 -0500

    Adding second smiling face

--- app/src/components/TodoCard.js
+++ app/src/components/TodoCard.js
@@ -19,7 +19,7 @@ const TodoCard = () => {
   //     };
   //   }
 
-  return <div className='emCrdCt'>The todo Card :D</div>;
+  return <div className='emCrdCt'>The todo Card :D :D</div>;
 };
 
 export default TodoCard;
